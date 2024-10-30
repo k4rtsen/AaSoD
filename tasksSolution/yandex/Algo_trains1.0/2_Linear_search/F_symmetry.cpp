97046ae633f53d83abd07cf0679b9efbb6b161fb
KartsenLT <karen.ayv12599@gmail.com>
2024-10-30 17:10:37 +0300

    new algorithm of finding simmetry in O(N)

--- tasksSolution/yandex/Algo_trains1.0/2_Linear_search/F_symmetry.cpp
+++ tasksSolution/yandex/Algo_trains1.0/2_Linear_search/F_symmetry.cpp
@@ -1,49 +1,12 @@
-// написать через префиксы
+// new solution O(N)
 #include <iostream>
 #include <vector>
 using namespace std;
 
-class HashStr
-{
-private:
-    int _x{257}, p{1'000'000'007};
-    vector<long long> hash;
-    vector<long long> reverse_hash;
-    vector<long long> x;
-
-public:
-    HashStr(vector<long long>);
-    bool isEqual(int, int);
-};
-
-HashStr::HashStr(vector<long long> arr)
-{
-    hash.push_back(0);
-    reverse_hash.push_back(0);
-    x.push_back(1);
-
-    for (int i = 0; i < arr.size(); i++)
-    {
-        hash.push_back((hash[i] * x[i] + arr[i]) % p);
-        reverse_hash.push_back((reverse_hash[i] * x[i] + arr[arr.size() - i - 1]) % p);
-        x.push_back((x[i] * _x) % p);
-    }
-}
-
-bool HashStr::isEqual(int len, int from)
-{
-    long long left{0};
-    long long right{0};
-    left = hash[len + from];
-    right = (reverse_hash[len] + hash[from] * x[len]) % p;
-
-    return left == right;
-}
-
 int main(int argc, char const *argv[])
 {
-    int count{0};
-    cin >> count;
+    int size{0};
+    cin >> size;
 
     int val{0};
     vector<long long> arr;
@@ -52,22 +15,39 @@ int main(int argc, char const *argv[])
         arr.push_back(val);
     }
 
-    int m{0};
-    HashStr hash(arr);
-    for (int i = 0; i < arr.size(); i++)
+    int left{0}, count{0};
+    int right{size - 1};
+
+    while (left < right)
     {
-        if (hash.isEqual(arr.size() - i, i))
+        if (arr[left] == arr[right])
         {
-            m = i;
-            break;
+            right--;
+            count++;
         }
+        else
+        {
+            right = size - 1;
+            if (count)
+            {
+                count = 0;
+                continue;
+            }
+            count = 0;
+        }
+
+        left++;
     }
 
-    cout << m << endl;
-    for (; m > 0; --m)
-    {
-        cout << arr[m] << " ";
-    }
+    val = arr.size() - count * 2;
+    if (!count)
+        val--;
+    if (count && left == right)
+        val--;
+        
+    cout << val << endl;
+    for (int i = val - 1; i >= 0; i--)
+        cout << arr[i] << " ";
 
     return 0;
 }
