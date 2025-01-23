238d958ad2f14866890073de7f3e3b4fee59b848
KartsenPC <karen.ayv12599@gmail.com>
2025-01-23 22:30:56 +0300

    right solution of 1 3 I

--- tasksSolution/yandex/Algo_trains1.0/3_sets/I_polyglot.cpp
+++ tasksSolution/yandex/Algo_trains1.0/3_sets/I_polyglot.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <fstream>
-#include <vector>
 #include <set>
 using namespace std;
 
@@ -12,36 +11,51 @@ void printSet(const set<string> &vec)
         cout << val << endl;
 }
 
-/**
- * @brief deletes in s1 that are not common elements with s2
- *
- * @param s1 set of string
- * @param s2 set of string
- */
-void intersection(set<string> &s1, set<string> &s2)
+template <typename T>
+set<T> intersection(set<T> &s1, set<T> &s2)
 {
+    set<T> result;
+    for (auto &val : s1)
+    {
+        if (s2.count(val))
+            result.insert(val);
+    }
+    return result;
 }
 
 int main(int argc, char const *argv[])
 {
-    int n{0}; // count of students
-
     set<string> allLngs, commonLngs;
     ifstream in("input.txt");
     if (in.is_open())
     {
+        int n{0}; // count of students
         in >> n;
-        while (!in.eof()) // or we can set loop for in range [0, n)
+        forn(_, n)
         {
+            set<string> currLngs;
             int m{0}; // count of language
+            in >> m;
             forn(_, m)
             {
                 string lng{}; // language
+                in >> lng;
                 allLngs.insert(lng);
+                currLngs.insert(lng);
             }
+
+            if (_ == 0)
+                commonLngs = currLngs;
+            else
+                commonLngs = intersection(commonLngs, currLngs);
         }
     }
     in.close();
+
+    cout << commonLngs.size() << endl;
+    printSet(commonLngs);
+    cout << allLngs.size() << endl;
+    printSet(allLngs);
 
     return 0;
 }
